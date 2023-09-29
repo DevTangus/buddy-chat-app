5ec1a05453d9d821860a90079f3a09483c91d7bb
Gilbert <kipngenogilbert96@gmai.com>
2023-09-29 13:16:56 +0300

    Added mutation and subscritpion resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,92 @@
 const resolvers = {
-Query: {
-user: ()=> User.find(),
-message: ()=> MessageChannel.find()
-},
-}
+    Query: {
+      user: () => User.find(),
+      message: () => Message.find(),
+    },
+    Mutation: { // Changed "Mutations" to "Mutation"
+      createUser: async (_, { name, email }) => {
+        const user = new User({ name, email });
+        await user.save();
+        createPubSub.publish("newUser", { newUser: user });
+        return user;
+      },
+  
+      updateUser: async (_, { id, name }) => {
+        const user = await User.findOneAndUpdate(
+          { _id: id },
+          { name },
+          { new: true }
+        );
+        return user;
+      },
+  
+      deleteUser: async (_, { email }) => {
+        await Promise.all([
+          User.findOneAndDelete({ email: email }),
+          Message.deleteMany({ senderMail: email }),
+        ]);
+        pubsub.publish("oldUser", { oldUser: email }); // Fixed typo "pubsub"
+        return true;
+      },
+  
+      userTyping: (_, { email, receiverMail }) => { // Fixed typo "enail" to "email"
+        pubsub.publish("userTyping", { userTyping: email, receiverMail });
+        return true;
+      },
+  
+      createMessage: async (_, { senderMail, receiverMail, message, timestamp }) => {
+        const userText = new Message({ senderMail, receiverMail, message, timestamp });
+        await userText.save();
+        pubsub.publish("newMessage", { newMessage: userText, receiverMail: receiverMail });
+        return userText;
+      },
+  
+      updateMessage: async (_, { id, message }) => {
+        const userText = await Message.findOneAndUpdate(
+          { _id: id },
+          { message },
+          { new: true }
+        );
+        return userText;
+      },
+  
+      deleteMessage: async (_, { id }) => {
+        await Message.findOneAndDelete({ _id: id });
+        return true;
+      },
+    },
+  };
+  
+  //  Subscription resolvers
+
+
+  const Subscription = {
+    newMessage: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator("newMessage"),
+        (payload, variables) => {
+          return payload.receiverMail === variables.receiverMail;
+        }
+      ),
+    },
+    newUser: {
+      subscribe: (_, {}, { pubsub }) => {
+        return pubsub.asyncIterator("newUser");
+      },
+    },
+    oldUser: {
+      subscribe: (_, {}, { pubsub }) => {
+        return pubsub.asyncIterator("oldUser");
+      },
+    },
+    userTyping: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator("userTyping"),
+        (payload, variables) => {
+          return payload.receiverMail === variables.receiverMail;
+        }
+      ),
+    },
+  };
+
+  
